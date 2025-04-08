1aac2f01b57eb2c5584d8305bdfc28ddf0b56f34
Shreeharsh <Shreeharshshinde006@gmail.com>
2025-04-08 12:41:37 +0530

    practice

---
+++ OddWhile.java
@@ -0,0 +1,18 @@
+import java.util.*;
+ 
+public class OddWhile{
+
+public static void main(String x[]){
+
+Scanner sc=new Scanner (System.in);
+
+int i=1;
+while(i<=100){
+
+System.out.println(i);
+i+=2;
+
+}
+}
+}
+
